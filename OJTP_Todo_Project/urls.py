df94a816d680be146b90b27326398f14b7155e05
goojjq648 <goojjq648@gmail.com>
2024-08-25 14:38:31 +0800

    feat: 修改todo url

--- OJTP_Todo_Project/urls.py
+++ OJTP_Todo_Project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todo/', include('todoapp.urls')),
+    path('', include('todoapp.urls')),
 ]
